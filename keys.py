8a29718af80927dd5f8a5b4aeddf0b687d2bb890
Brandon Smith <blsmit5728@gmail.com>
2023-06-15 13:51:23 -0400

    Config easier

--- keys.py
+++ keys.py
@@ -1,7 +1,14 @@
 import numpy
 from shapely.geometry import Point, Polygon
 import json
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 import simplekml
+
+config_parms = load(open("config.yml", "r"),Loader=Loader)
 
 fn = "jac_keys.csv"
 fd = open(fn, "r")
@@ -28,34 +35,22 @@ keys = export_keys_dict.get("keys")
 
 keys_in_poly = {"keys" : [] }
 
-CharlestonBox = [
-        (38.3374823, -81.6950226),
-        (38.3544457, -81.7084122),
-        (38.3738276, -81.7171669),
-        (38.3805562, -81.7042923),
-        (38.3829784, -81.6692734),
-        (38.3700593, -81.6431808),
-        (38.3587533, -81.6196632),
-        (38.3462338, -81.6043854),
-        (38.3396366, -81.5899658),
-        (38.3291337, -81.5676498),
-        (38.3164744, -81.5535736),
-        (38.3066418, -81.5501404),
-        (38.2999063, -81.5640450),
-        (38.2984244, -81.5803528),
-        (38.3233431, -81.6122818),
-        (38.3312883, -81.6388893),
-        (38.3416562, -81.6536522),
-        (38.3477147, -81.6682434),
-        (38.3369437, -81.6948509),
-        (38.3374823, -81.6950226)
-    ]
+bbox_coords = config_parms.get("bbox")
+print (bbox_coords)
+BoundingBox = []
+for vertex in bbox_coords:
+    LAT, LON = vertex.split(",")
+    LAT = float(LAT)
+    LON = float(LON)
+    BoundingBox.append( (LAT, LON) )
+print(BoundingBox)
 
-CharlestonPolygon = Polygon(CharlestonBox)
+
+POLYGON = Polygon(BoundingBox)
 
 for key in keys:    
     point = Point(key.get("lat"),key.get("lng"))    
-    if CharlestonPolygon.contains(point):
+    if POLYGON.contains(point):
         print("----", key)
         keys_in_poly["keys"].append(key)
     
